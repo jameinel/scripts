255b69f20ba7a29604eac8717972d2c0567b02c4
John Arbash Meinel <john@arbash-meinel.com>
2015-01-21 09:46:10 +0400

    start working on retry logic. both Toku and FoundationDB give us different errors that should mean that we should retry the transaction.

--- benchmark-insert.go
+++ benchmark-insert.go
@@ -14,6 +14,7 @@ var (
 	URL         = flag.String("mongo", "localhost", "URL for dialing mongo")
 	Bulk        = flag.Bool("bulk", false, "use a single bulk call")
 	Count       = flag.Int("count", 1000, "number to insert")
+	MaxRetry    = flag.Int("max-retry", 3, "retry an operation at most this many times")
 	Start       = flag.Int("start", 0, "starting offset")
 	Unsafe      = flag.Bool("unsafe", false, "set safe mode to non-blocking and not-checking errors")
 	Sync        = flag.Bool("sync", false, "Force an fsync for writes to be considered committed (ignored if Unsafe is set)")
@@ -23,6 +24,44 @@ var (
 	Verify      = flag.Bool("verify", false, "After inserting, verify all documents are correct and new docs are inserted.")
 	Verbose     = flag.Bool("verbose", false, "Print more information.")
 )
+
+var RetryCount = 0
+
+const ( // error codes
+	fdbOperationFailed = 1000
+	// These are all codes that "should be retried"
+	fdbTimedOut      = 1004
+	fdbPastVersion   = 1007
+	fdbFutureVersion = 1009
+	fdbNotCommitted  = 1020
+	// Toku Error Codes that can be retried
+	tokuLockNotGranted = 16759
+)
+
+var retryCodes = map[int]bool{
+	fdbTimedOut:        true,
+	fdbPastVersion:     true,
+	fdbFutureVersion:   true,
+	fdbNotCommitted:    true,
+	tokuLockNotGranted: true,
+}
+
+func isCodeRetry(code int) bool {
+	return retryCodes[code]
+}
+
+func shouldRetry(err error) bool {
+	if err == nil {
+		return false
+	}
+	if mgoErr, ok := err.(*mgo.LastError); ok {
+		return isCodeRetry(mgoErr.Code)
+	}
+	if mgoErr, ok := err.(*mgo.QueryError); ok {
+		return isCodeRetry(mgoErr.Code)
+	}
+	return false
+}
 
 func transactionCommand(db *mgo.Database, cmd interface{}) error {
 	var result bson.M
@@ -35,7 +74,10 @@ func transactionCommand(db *mgo.Database, cmd interface{}) error {
 	return nil
 }
 
-func maybeBegin(db *mgo.Database, cond bool) error {
+func maybeBegin(db *mgo.Database, cond bool, retries *int) error {
+	//maybeBegin takes 'retries' because we know no other work has been
+	//done that we might want to clean up. so we just retry this specific
+	//operation.
 	if cond {
 		// FoundationDB has a built-in 5s timeout for find() operations
 		// operating inside of a transaction. So just set the max
@@ -43,9 +85,19 @@ func maybeBegin(db *mgo.Database, cond bool) error {
 		// running in single insert mode.
 		// We set it to 6s so that we can see past_version failures
 		// rather than transaction_timeout ones.
-		if err := transactionCommand(db, map[string]interface{}{"beginTransaction": 1, "timeout": 6000}); err != nil {
-			fmt.Printf("failed to beginTransaction: %s\n", err)
-			return err
+		for {
+			if err := transactionCommand(db, map[string]interface{}{"beginTransaction": 1, "timeout": 6000}); err != nil {
+				fmt.Printf("failed to beginTransaction: %s\n", err)
+				if shouldRetry(err) {
+					if retries != nil && *retries < *MaxRetry {
+						*retries++
+						RetryCount++
+						continue
+					}
+				}
+				return err
+			}
+			return nil
 		}
 	}
 	return nil
@@ -74,34 +126,45 @@ func maybeRollback(db *mgo.Database, cond bool) error {
 
 func oneByOneDirect(db *mgo.Database) error {
 	collection := db.C("foo")
-	if err := maybeBegin(db, *OneTrans); err != nil {
+	var retries int = 0
+	if err := maybeBegin(db, *OneTrans, &retries); err != nil {
 		return err
 	}
 	for i := 0; i < *Count; i++ {
 		val := *Start + i
 		strVal := fmt.Sprintf("%07d", val)
-		if err := maybeBegin(db, *EachTrans); err != nil {
-			return err
-		}
-		if err := collection.Insert(bson.M{"_id": strVal, "val": strVal}); err != nil {
-			fmt.Printf("could not insert %d\n%s\n", val, err)
-			// ignore the error because we are returning anyway
-			maybeRollback(db, *OneTrans || *EachTrans)
-			return err
-		}
-		if err := maybeCommit(db, *EachTrans); err != nil {
-			return err
+		// reset retries per document
+		for retries = 0; retries < *MaxRetry; retries++ {
+			if err := maybeBegin(db, *EachTrans, &retries); err != nil {
+				return err
+			}
+			if err := collection.Insert(bson.M{"_id": strVal, "val": strVal}); err != nil {
+				fmt.Printf("could not insert doc 1 %d\n%s\n", val, err)
+				// ignore the error because we are returning anyway
+				maybeRollback(db, *OneTrans || *EachTrans)
+				if shouldRetry(err) {
+					RetryCount += 1
+					continue
+				}
+				return err
+			}
+			if err := maybeCommit(db, *EachTrans); err != nil {
+				if shouldRetry(err) {
+					RetryCount += 1
+					continue
+				}
+				return err
+			} else {
+				break
+			}
 		}
 	}
-	// ignore the error because we are returning anyway
 	return maybeCommit(db, *OneTrans)
 }
 
 func bulkDirect(db *mgo.Database) error {
 	collection := db.C("foo")
-	if err := maybeBegin(db, *OneTrans || *EachTrans); err != nil {
-		return err
-	}
+	var retries int = 0
 	docs := make([]interface{}, 0, *Count)
 	for i := 0; i < *Count; i++ {
 		val := *Start + i
@@ -111,14 +174,31 @@ func bulkDirect(db *mgo.Database) error {
 	if *Verbose {
 		fmt.Printf("First doc: %v\n", docs[0])
 	}
-	if err := collection.Insert(docs...); err != nil {
-		fmt.Printf("could not insert all docs\n%s\n", err.Error())
-		// ignore the error because we are returning anyway
-		maybeRollback(db, *OneTrans || *EachTrans)
-		return err
+	for ; retries < *MaxRetry; retries++ {
+		if err := maybeBegin(db, *OneTrans || *EachTrans, &retries); err != nil {
+			return err
+		}
+		if err := collection.Insert(docs...); err != nil {
+			fmt.Printf("could not insert all docs\n%s\n", err.Error())
+			// ignore the error because we are returning anyway
+			maybeRollback(db, *OneTrans || *EachTrans)
+			if shouldRetry(err) {
+				RetryCount += 1
+				continue
+			}
+			return err
+		}
+		if err := maybeCommit(db, *OneTrans || *EachTrans); err != nil {
+			if shouldRetry(err) {
+				RetryCount += 1
+				continue
+			}
+			return err
+		} else {
+			break
+		}
 	}
-	// ignore the error because we are returning anyway
-	return maybeCommit(db, *OneTrans || *EachTrans)
+	return nil
 }
 
 func oneDocOp(val int) txn.Op {
@@ -133,25 +213,39 @@ func oneDocOp(val int) txn.Op {
 
 func oneByOneClientTrans(db *mgo.Database) error {
 	txnCollection := db.C("transactions")
-	if err := maybeBegin(db, *OneTrans); err != nil {
+	var retries int = 0
+	if err := maybeBegin(db, *OneTrans, &retries); err != nil {
 		return err
 	}
 	for i := 0; i < *Count; i++ {
 		val := *Start + i
-		if err := maybeBegin(db, *EachTrans); err != nil {
-			return err
-		}
-		runner := txn.NewRunner(txnCollection)
-		op := oneDocOp(val)
-		txnId := bson.NewObjectId()
-		if err := runner.Run([]txn.Op{op}, txnId, nil); err != nil {
-			fmt.Printf("could not insert %d\n%s\n", val, err)
-			// ignore the error because we are returning anyway
-			maybeRollback(db, *OneTrans || *EachTrans)
-			return err
-		}
-		if err := maybeCommit(db, *EachTrans); err != nil {
-			return err
+		// Reset retries per document
+		for retries = 0; retries < *MaxRetry; retries++ {
+			if err := maybeBegin(db, *EachTrans, &retries); err != nil {
+				return err
+			}
+			runner := txn.NewRunner(txnCollection)
+			op := oneDocOp(val)
+			txnId := bson.NewObjectId()
+			if err := runner.Run([]txn.Op{op}, txnId, nil); err != nil {
+				fmt.Printf("could not insert %d\n%s\n", val, err)
+				// ignore the error because we are returning anyway
+				maybeRollback(db, *OneTrans || *EachTrans)
+				if shouldRetry(err) {
+					RetryCount += 1
+					continue
+				}
+				return err
+			}
+			if err := maybeCommit(db, *EachTrans); err != nil {
+				if shouldRetry(err) {
+					RetryCount += 1
+					continue
+				}
+				return err
+			} else {
+				break
+			}
 		}
 	}
 	// ignore the error because we are returning anyway
@@ -160,23 +254,39 @@ func oneByOneClientTrans(db *mgo.Database) error {
 
 func bulkClientTrans(db *mgo.Database) error {
 	txnCollection := db.C("transactions")
+	var retries int = 0
 	ops := make([]txn.Op, 0, *Count)
-	if err := maybeBegin(db, *OneTrans || *EachTrans); err != nil {
-		return err
-	}
 	for i := 0; i < *Count; i++ {
 		ops = append(ops, oneDocOp(*Start+i))
 	}
-	runner := txn.NewRunner(txnCollection)
-	txnId := bson.NewObjectId()
-	if err := runner.Run(ops, txnId, nil); err != nil {
-		fmt.Printf("could not insert all\n%s\n", err)
+	for ; retries < *MaxRetry; retries++ {
+		if err := maybeBegin(db, *OneTrans || *EachTrans, &retries); err != nil {
+			return err
+		}
+		runner := txn.NewRunner(txnCollection)
+		txnId := bson.NewObjectId()
+		if err := runner.Run(ops, txnId, nil); err != nil {
+			fmt.Printf("could not insert all\n%s\n", err)
+			// ignore the error because we are returning anyway
+			maybeRollback(db, *OneTrans || *EachTrans)
+			if shouldRetry(err) {
+				RetryCount += 1
+				continue
+			}
+			return err
+		}
 		// ignore the error because we are returning anyway
-		maybeRollback(db, *OneTrans || *EachTrans)
-		return err
+		if err := maybeCommit(db, *OneTrans || *EachTrans); err != nil {
+			if shouldRetry(err) {
+				RetryCount += 1
+				continue
+			}
+			return err
+		} else {
+			break
+		}
 	}
-	// ignore the error because we are returning anyway
-	return maybeCommit(db, *OneTrans || *EachTrans)
+	return nil
 }
 
 type ValDoc struct {
@@ -237,9 +347,9 @@ func main() {
 		}
 	}
 	if err != nil {
-		fmt.Printf("%.3fms to insert %d documents (failed: %s)\n", float64(time.Since(start))/float64(time.Millisecond), *Count, err)
+		fmt.Printf("%.3fms to insert %d documents (%d retries, FAILED: %s)\n", float64(time.Since(start))/float64(time.Millisecond), *Count, RetryCount, err)
 	} else {
-		fmt.Printf("%.3fms to insert %d documents\n", float64(time.Since(start))/float64(time.Millisecond), *Count)
+		fmt.Printf("%.3fms to insert %d documents (%d retries)\n", float64(time.Since(start))/float64(time.Millisecond), *Count, RetryCount)
 	}
 	if *Verify {
 		if err := verifyNewDocs(db); err != nil {
